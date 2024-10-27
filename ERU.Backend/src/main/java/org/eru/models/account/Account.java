f0829986a273a46a548c09ddee6eec1ed7399f11
Josh <joshua.audia@gmail.com>
2024-10-26 22:17:43 -0500

    DataBase structure for likes/comments/accounts

--- ERU.Backend/src/main/java/org/eru/models/account/Account.java
+++ ERU.Backend/src/main/java/org/eru/models/account/Account.java
@@ -3,14 +3,21 @@ package org.eru.models.account;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class Account {
     @JsonProperty("id")
     public String Id;
+
+    @JsonProperty("name")
+    public String Name;
 
     @JsonProperty("displayName")
     public String DisplayName;
 
     @JsonProperty("externalAuths")
     public HashMap<String, String> ExternalAuths = new HashMap<>();
+
+    @JsonProperty("posts")
+    public List<Post> Posts;
 }
