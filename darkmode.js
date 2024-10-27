31353e311c4916535e5c69a1aaf936f735b809b1
Sabua204 <sabual2@lsu.edu>
2024-10-26 22:51:44 -0500

    Theme Switch Button

---
+++ darkmode.js
@@ -0,0 +1,29 @@
+//Unsure why, but themes wont switch properly. Need to look into it more.
+
+let darkmode= localStorage.getItem('darkmode')
+const themeSwitch= document.getElementById('theme-switch')
+
+//enables darkmode
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+//removes dark mode
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+}
+
+if(darkmode == "active") 
+    enableDarkmode()
+
+
+dark_mode_toggle.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    if(darkmode !== "active"){
+        enableDarkmode()
+    }else{
+        disableDarkmode()
+    }
+})
