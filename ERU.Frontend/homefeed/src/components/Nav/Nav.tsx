d68b24bdfb9d8cc18767383c730adbd68f30cd5c
nrodrigue2004 <nrodr34@lsu.edu>
2024-11-23 21:47:20 -0600

    Changed js to ts

--- ERU.Frontend/homefeed/src/components/Nav/Nav.tsx
+++ ERU.Frontend/homefeed/src/components/Nav/Nav.tsx
@@ -1,81 +1,46 @@
-/* tutorial referenced: https://youtu.be/D8fdAsIxDh8?si=2R3RB-d9MX9b1--O */
+import React from "react"; // Import React for JSX syntax and components
+import styles from "./Nav.module.css"; // Import CSS module for styling
+import data from "./links.json"; // Import navigation links from JSON file
 
-/* Importing React library to use JSX syntax and create components */
-import React from 'react';
-
-import { Link } from 'react-router-dom';
-
-/* Importing CSS styles from Nav.module.css for styling the navigation bar */
-import styles from './Nav.module.css';
-
-/* Importing data from links.json file which contains the navigation links */
-import * as data from './links.json';
-import logo from '../assets/logo.png';
-
-
-
-
-/* Retrieve the 'links' array from the JSON data
-* Convert the imported data into a string and then parse it back into an object */
-const linksString = JSON.stringify(data);
-const links = JSON.parse(linksString).links;
-
-
-/* Define a type (string) for each link, which has a label and href
-* label (string) = the text displayed for the link
-* href (string) = the URL the link points to */
-type Link = {
-    label: string;
-    href: string;
+// Define TypeScript interfaces
+interface Link {
+  href: string;
+  label: string;
 }
 
-/* Functional component to render the list of links */
-const Links: React.FC<{ links: Link[] }> = function({ links }) {
-    /* Returning a div containing each link rendered as a clickable item */
-    return (
-        <div className={styles['links-container']}>
-            {links.map(function(link: Link) {
-                /* Each link is displayed in a div with a unique key (href) */
-                return (
-                    <div key={link.href} className={styles['link']}>
-                        <Link to={link.href}>
-                            {link.label} {/* Displaying the label for the link */}
-                        </Link>
-                    </div>
-                );
-            })}
+interface LinksProps {
+  links: Link[];
+}
+
+// Logo reference (assumes it is in the public folder)
+const logo = "/logo.png";
+
+// Functional component to render the list of navigation links
+const Links: React.FC<LinksProps> = ({ links }) => {
+  return (
+    <div className={styles["links-container"]}>
+      {links.map((link) => (
+        <div key={link.href} className={styles["link"]}>
+          <a href={link.href}>{link.label}</a>
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
-/* Main functional component for the nav bar*/
-const Nav: React.FC = function() {
-    /* Render the navigation bar with a logo and links */
-    return (
-        <nav className={styles.navbar}>
-            <div className={styles['logo-container']}>
-            <img src={logo} alt="logo" className={styles.logo} /> {/* Logo image */}
-            </div>
-            <Links links={links} /> {/* Render the Links component with the links data */}
-        </nav>
-    );
-}
+// Main navigation bar component
+const Nav: React.FC = () => {
+  // Extract the 'links' array from the JSON data
+  const links: Link[] = data.links;
 
-/* Exporting the Nav component for use in other parts of the application */
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles["logo-container"]}>
+        <img src={logo} alt="logo" className={styles.logo} />
+      </div>
+      <Links links={links} />
+    </nav>
+  );
+};
+
 export default Nav;
-
-/* link.json comments
-
-tutorial referenced: https://youtu.be/D8fdAsIxDh8?si=2R3RB-d9MX9b1--O
-
-{
-  (Key contains an array of link objects)
-  "links": [
-    {
-      ("label" represents the link text)
-      "label": "Home",
-      ("href" represents the URL the link points to)
-      "href": "/home"
-    }
-  ]
-} */
