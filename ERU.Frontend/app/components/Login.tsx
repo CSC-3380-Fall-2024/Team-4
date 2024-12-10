f263dd1f971ad5bc87630808c58aac36dc2284de
Josh <joshua.audia@gmail.com>
2024-12-09 23:13:18 -0600

    Added basic login and register page

---
+++ ERU.Frontend/app/components/Login.tsx
@@ -0,0 +1,34 @@
+import './Login.css';
+
+const Login = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="screen">
+                    <div className="screen__content">
+                        <form className="login">
+                            <div className="login__field">
+                                <input type="text" className="login__input" placeholder="Username"/>
+                            </div>
+                            <div className="login__field">
+                                <input type="password" className="login__input" placeholder="Password"/>
+                            </div>
+                            <button className="button login__submit">
+                                <span className="button__text">Log In</span>
+                            </button>
+                            <a className="switch__login" href="">Register</a>
+                        </form>
+                    </div>
+                    <div className="screen__background">
+                        <span className="screen__background__shape screen__background__shape4"></span>
+                        <span className="screen__background__shape screen__background__shape3"></span>		
+                        <span className="screen__background__shape screen__background__shape2"></span>
+                        <span className="screen__background__shape screen__background__shape1"></span>
+                    </div>		
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Login;
--- ERU.Frontend/app/index.tsx
+++ ERU.Frontend/app/index.tsx
@@ -3,13 +3,13 @@ import { AppRegistry } from 'react-native';
 import { View, Text, StyleSheet } from 'react-native';
 import { expo as expoRoot } from '../app.json';
 
-import HomeFeed from './homefeed';
+import Login from './components/Login';
 
 const appName : string = expoRoot.name;
 
 const Home: React.FC = () => {
   return (
-    <HomeFeed/>
+    <Login/>
   );
 };
 
